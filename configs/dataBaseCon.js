ac9d8334b17bc9e406b43b20accea05652aa648f
Vlad <chinnov.vlad@gmail.com>
2023-05-05 10:42:30 +0300

    измеил подключение к бд

--- configs/dataBaseCon.js
+++ configs/dataBaseCon.js
@@ -1,22 +1,34 @@
 const { MongoClient } = require('mongodb');
-const uri = 'mongodb://localhost:27017'; 
+const uri = 'mongodb://127.0.0.1:27017'; 
 const client = new MongoClient(uri);
+
+function connectToDb (){
+    client.connect();
+    console.log('Connected...');
+    const db = client.db('sapr_labs'); 
+    return db;
+}
+db = connectToDb()
+
+console.log(db);
+
+
 //Подключение к бд и возвращения объекта базы данных
 
-function connectToMongoDB() {
-    return new Promise((resolve, reject) => {
-    client.connect((error) => {
-        if (error) {
-            console.error('Error connecting to MongoDB', error);
-            reject(error);
-        } else {
-            console.log('Connected to MongoDB');
-          // Получение объекта базы данных
-            const data = client.db('sapr_labs'); 
-            resolve(data);
-        }
-        });
-    });
-}
+// function connectToDb() {
+//     return new Promise((resolve, reject) => {
+//     client.connect((error) => {
+//         if (error) {
+//             console.error('Error connecting to MongoDB', error);
+//             reject(error);
+//         } else {
+//             console.log('Connected to MongoDB');
+//           // Получение объекта базы данных
+//             const data = client.db('sapr_labs'); 
+//             resolve(data);
+//         }
+//         });
+//     });
+// }
 
-module.exports = connectToMongoDB;
+module.exports = connectToDb;
