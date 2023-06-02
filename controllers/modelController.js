f7814f0ba95f0dc9c26f4f93af38a308bce69bc5
Vlad <chinnov.vlad@gmail.com>
2023-06-02 17:59:19 +0300

    создал контроллер моделей

--- controllers/modelController.js
+++ controllers/modelController.js
@@ -1,6 +1,13 @@
 const { getDataFromDatabase,
-        addNewData,
-        getOneData} = require('../services/modelService');
+    addNewData,
+    getOneData,
+    deleteDate,
+    updateData } = require('../services/modelService');
+const { getName } = require('../services/apiService');
+const { ObjectId } = require("mongodb");
+const { BadRequest } = require('../utils/errors');
+
+//post с параметром - проверяем APIKEY в заголовке если успешно то продолжаем, всё эо через middleware
 
 async function getModels(req, res) {
     try {
@@ -11,23 +18,75 @@ async function getModels(req, res) {
     }
 }
 
-async function getModelByID(req, res) {
+async function getModelByID(req, res, next) {
+    const idModel = req.params.id;
+    if (idModel === undefined) {
+        next(new BadRequest('invalid id'));
+    }
     try {
-        const result = await getOneData(req.params.id);
+        const result = await getOneData(idModel);
         res.json(result);
+        next();
     } catch (error) {
         res.status(404).json({ error: 'Not Found' });
+        next(error);
     }
 }
 
 async function addNewItemModel(req, res) {
-    const dataCreated = new Date();
-    const {name, model_name,type_of_model,object_model,description,comments } = req.body;
+    //берём имя по токену из коллекции ключей и добавляем его в post запрос
     try {
-        await addNewData("models",{name, model_name,type_of_model,object_model,description,comments, dataCreated});
+        const apiKey = req.headers.api;
+        const data = await getName(apiKey);
+        const { name } = data;
+        const dataCreated = new Date();
+        const { model_name, type_of_model, object_model, description, comments } = req.body;
+        if (name === undefined) {
+            next(new NotFound("Not Found name"));
+        }
+        else if (!apiKey) {
+            next(new BadRequest("Invalid API"));
+        }
+        else {
+            await addNewData("models", { name, model_name, type_of_model, object_model, description, comments, dataCreated });
+        }
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
+        next(error);
     }
 }
 
-module.exports = {addNewItemModel,getModels,getModelByID};
+async function deleteModelByID(req, res, next) {
+    const id = req.params.id;
+    try {
+        if (!id) {
+            next(new BadRequest("Invalid id"));
+        }
+        else {
+            await deleteDate("models", id);
+            next();
+        }
+    } catch (error) {
+        res.status(404).json({ error: 'Not Found' });
+        next(error);
+    }
+}
+async function updateModelById(req, res, next) {
+    const idModel = req.params.id;
+    const dateUpdate = new Date();
+    try {
+        if(!idModel) {
+            next(new BadRequest("Invalid id"));
+        }else{
+            const result = await updateData('models', idModel, dateUpdate);
+            res.json(result);
+            next();
+        }
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+
+module.exports = { addNewItemModel, getModels, getModelByID, deleteModelByID, updateModelById };
